706194e7959614d34b9bd3a57005ae20f0c89bb3
Beqael <beqaelashvili3@gmail.com>
2024-10-03 18:03:34 +0400

    add_second_page

--- src/app/Components/Main/Main.tsx
+++ src/app/Components/Main/Main.tsx
@@ -2,56 +2,81 @@ import { motion } from "framer-motion";
 
 export function Main() {
   return (
-    <div className="relative flex items-center justify-center">
-      <img
-        className="w-full object-contain"
-        src="/mclaren.jpeg"
-        alt="mclaren"
-      />
-      <div className="absolute w-full text-white flex flex-col gap-16 ">
-        <div className="relative w-full flex flex-col text-white">
-          <p className="bg-white absolute top-[26px] w-full h-px"></p>
-          <motion.p
-            whileInView={{ opacity: 1, x: 0 }}
-            initial={{ opacity: 0, x: -100 }}
-            transition={{ duration: 0.5 }}
-            className="text-3xl lg:text-9xl ml-36"
-          >
-            THE GOLD
-          </motion.p>
-          <p className="bg-white absolute bottom-[10px] w-full h-px"></p>
-        </div>
+    <div>
+      <div className="relative flex items-center justify-center">
         <div className="relative w-full">
-          <p className="bg-white absolute mt-[22px] w-full h-px"></p>
-          <motion.p
-            whileInView={{ opacity: 1, x: 0 }}
-            initial={{ opacity: 0, x: -100 }}
-            transition={{ duration: 1 }}
-            className="flex justify-between px-14 text-9xl"
-          >
-            Standards{" "}
-            <motion.span
-              whileInView={{ opacity: 1, x: 0 }}
-              initial={{ opacity: 0, x: 150 }}
-              transition={{ duration: 1 }}
-              className="mr-40"
-            >
-              in
-            </motion.span>
-          </motion.p>
-          <p className="bg-white absolute bottom-[10px] w-full h-px"></p>
+          <img
+            className="w-full object-contain"
+            src="/mclaren.jpeg"
+            alt="mclaren"
+          />
+          <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black"></div>
         </div>
-        <div className="relative">
-          <p className="bg-white absolute w-full top-[26px] h-px"></p>
-          <motion.p
-            whileInView={{ opacity: 1, x: 0 }}
-            initial={{ opacity: 0, x: 100 }}
-            transition={{ delay: 0.5, duration: 1 }}
-            className="text-9xl float-end mr-36"
-          >
-            Luxury car
-          </motion.p>
-          <p className="bg-white absolute bottom-[10px] w-full h-px"></p>
+        <div className="absolute w-full text-white flex flex-col gap-16 ">
+          <div className="relative w-full flex flex-col text-white">
+            <p className="bg-white absolute top-[26px] w-full h-px"></p>
+            <motion.p
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 0.5 }}
+              className="text-3xl lg:text-9xl ml-36"
+            >
+              THE GOLD
+            </motion.p>
+            <p className="bg-white absolute bottom-[10px] w-full h-px"></p>
+          </div>
+          <div className="relative w-full">
+            <p className="bg-white absolute mt-[22px] w-full h-px"></p>
+            <motion.p
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 1 }}
+              className="flex justify-between px-14 text-9xl"
+            >
+              Standards{" "}
+              <motion.span
+                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: 150 }}
+                transition={{ duration: 1 }}
+                className="mr-40"
+              >
+                in
+              </motion.span>
+            </motion.p>
+            <p className="bg-white absolute bottom-[10px] w-full h-px"></p>
+          </div>
+          <div className="relative">
+            <p className="bg-white absolute w-full top-[26px] h-px"></p>
+            <motion.p
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: 100 }}
+              transition={{ delay: 0.5, duration: 1 }}
+              className="text-9xl float-end mr-36"
+            >
+              Luxury car
+            </motion.p>
+            <p className="bg-white absolute bottom-[10px] w-full h-px"></p>
+          </div>
+        </div>
+      </div>
+      <div className="bg-black text-white w-full">
+        <div className="px-2 py-40 flex justify-between ">
+          <div className="flex flex-col gap-20">
+            <h1>RENTAL CARS</h1>
+            <p className="w-3/4 text-6xl ">Experience is everything</p>
+          </div>
+          <p className=" w-2/6  flex items-end">
+            Whether you're looking to elevate your business transportation,
+            seeking a wedding getaway car, or are just ready to have an
+            exhilarating experience, there is no better answer than Rental Cars.
+          </p>
+        </div>
+        <div className="inline relative bg-red-200 h-[1000px]">
+          <img className="object-contain" src="/porche.jpg" alt="" />
+          <div className="absolute inset-0 bg-gradient-to-t from-transparent to-black"></div>
+        </div>
+        <div className="px-2 h-12">
+          <h1 className="flex items-end">Our Featured Rides</h1>
         </div>
       </div>
     </div>
