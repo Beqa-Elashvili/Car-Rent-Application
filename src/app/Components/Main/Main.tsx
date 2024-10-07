e803c077a582e3189256aad6e0c2901bbb8bac75
Beqael <beqaelashvili3@gmail.com>
2024-10-07 13:51:27 +0400

    add_next-page

--- src/app/Components/Main/Main.tsx
+++ src/app/Components/Main/Main.tsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { SiLamborghini } from "react-icons/si";
 
 export function Main() {
   return (
@@ -75,8 +76,51 @@ export function Main() {
           <img className="object-contain" src="/porche.jpg" alt="" />
           <div className="absolute inset-0 bg-gradient-to-t from-transparent to-black"></div>
         </div>
-        <div className="px-2 h-12">
-          <h1 className="flex items-end">Our Featured Rides</h1>
+        <h1 className="px-2 flex items-end">Our Featured Rides</h1>
+        <div className="relative flex flex-col items-center justify-center">
+          <img className="w-full" src="/lamborgini.jpeg" alt="" />
+          <div className="absolute  m-auto w-4/6">
+            <h1 className="text-2xl mb-40">LAMBORGHINI</h1>
+            <div className="flex items-center justify-between">
+              <div className="flex flex-col gap-20">
+                <h1 className="text-9xl">REVUELTO</h1>
+                <div className="flex gap-12 text-sm text-gray-400 font-medium">
+                  <div>
+                    <p>STARTING AT</p>
+                    <h3 className="text-white text-xl">$1500</h3>
+                  </div>
+                  <div>
+                    <p>TOP SPEED</p>
+                    <h3 className="text-white text-xl">350</h3>
+                  </div>
+                  <div>
+                    <p>0-100</p>
+                    <h3 className="text-white text-xl">2.5s</h3>
+                  </div>
+                  <div>
+                    <p>TORUE</p>
+                    <h3 className="text-white text-xl">661Hp</h3>
+                  </div>
+                  <div>
+                    <p>TRANSMISSION</p>
+                    <h3 className="text-white text-xl">7</h3>
+                  </div>
+                  <div>
+                    <p>SEAT</p>
+                    <h3 className="text-white text-xl">2</h3>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <button className="border-none rounded-full w-40 h-40 font-medium text-2xl bg-blue-500 hover:shadow-lg">
+                  View
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-4/6 relative h-40 flex flex-col overflow-hidden justify-center m-auto">
+          <SiLamborghini className="text-[200px] absolute" />
         </div>
       </div>
     </div>
