7a6bcce99dd102f4b73b5ebc8cd53019e9d23c3d
Beqael <beqaelashvili3@gmail.com>
2024-10-16 14:07:48 +0400

    finish_collection_site

--- src/app/Providers/GlobalProvider/GlobalContext.ts
+++ src/app/Providers/GlobalProvider/GlobalContext.ts
@@ -2,8 +2,15 @@ import { StaticImageData } from "next/dist/shared/lib/get-img-props";
 import { Dispatch, SetStateAction, createContext } from "react";
 
 interface GlobalContextProps {
-  collections: any[];
-  setCollections: Dispatch<SetStateAction<any[]>>;
+  collections: { name: string; logo: string }[];
+  setCollections: Dispatch<
+    SetStateAction<
+      {
+        name: string;
+        logo: string;
+      }[]
+    >
+  >;
 }
 
 export const GlobalContext = createContext<GlobalContextProps>({
--- src/app/pages/collection/page.tsx
+++ src/app/pages/collection/page.tsx
@@ -1,12 +1,27 @@
 "use client";
 import { useGlobalProvider } from "@/app/Providers/GlobalProvider";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
   const { collections } = useGlobalProvider();
+  const router = useRouter();
   return (
-    <div className="bg-gray-800 h-screen flex items-center justify-center flex-wrap">
-      {collections.map((item) => (
-        <img className="w-40" src={item.logo} alt="" />
+    <div className="bg-gray-800 h-screen flex items-center justify-center flex-wrap gap-2">
+      {collections.map((item, index) => (
+        <div
+          onClick={() => router.push(`/pages/brands/${item.name}`)}
+          className="p-2 flex flex-col items-center border object-contain rounded-xl transition duration-300 hover:bg-gray-500  hover:cursor-pointer"
+        >
+          <img
+            key={index}
+            className="h-60 w-60 object-contain"
+            src={item.logo}
+            alt="brand-logo"
+          />
+          <h1 className="text-3xl font-medium text-white">
+            {item.name.toUpperCase()}
+          </h1>
+        </div>
       ))}
     </div>
   );
