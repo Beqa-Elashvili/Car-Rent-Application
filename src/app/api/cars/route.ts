016c0aca91b5b3c65f30ce0933daf4fb35d01de7
Beqael <beqaelashvili3@gmail.com>
2024-12-08 14:22:11 +0400

    add_cars_in_database

--- src/app/api/cars/route.ts
+++ src/app/api/cars/route.ts
@@ -2,44 +2,60 @@ import mongoose from "mongoose";
 import { Cars } from "models/cars";
 import { NextResponse } from "next/server";
 
-export async function POST(req: Request) {
+export async function POST(req: NextResponse) {
   try {
-    const { car } = await req.json();
+    const { car, cars } = await req.json();
 
-    const newCar = new Cars({
-      city_mpg: car.city_mpg,
-      class: car.class,
-      combination_mpg: car.combination_mpg,
-      cylinders: car.cylinders,
-      displacement: car.displacement,
-      drive: car.drive,
-      horsepower: car.horsepower,
-      fuel_type: car.fuel_type,
-      highway_mpg: car.highway_mpg,
-      make: car.make,
-      brand: car.brand,
-      model: car.model,
-      transmission: car.transmission,
-      year: car.year,
-      carDayCount: car.carDayCount || 0,
-      dayPrice: car.dayPrice || 0,
-      img: car.img,
-    });
+    if (car) {
+      const newCar = new Cars({
+        city_mpg: car.city_mpg,
+        class: car.class,
+        combination_mpg: car.combination_mpg,
+        cylinders: car.cylinders,
+        displacement: car.displacement,
+        drive: car.drive,
+        horsepower: car.horsepower,
+        fuel_type: car.fuel_type,
+        highway_mpg: car.highway_mpg,
+        make: car.make,
+        brand: car.brand,
+        model: car.model,
+        transmission: car.transmission,
+        year: car.year,
+        carDayCount: car.carDayCount || 0,
+        dayPrice: car.dayPrice || 0,
+        img: car.img,
+      });
 
-    await newCar.save();
-
-    return NextResponse.json(
-      { message: "Car created successfully", car: newCar },
-      { status: 201 }
-    );
+      await newCar.save();
+      return NextResponse.json(
+        { message: "Car created successfully", car: newCar },
+        { status: 201 }
+      );
+    } else if (cars && Array.isArray(cars)) {
+      const newCars = await Cars.insertMany(cars);
+      return NextResponse.json(
+        { message: "Cars created successfully", cars: newCars },
+        { status: 201 }
+      );
+    } else {
+      return NextResponse.json(
+        {
+          message:
+            'Invalid request format. Provide either a "car" or an array of "cars".',
+        },
+        { status: 400 }
+      );
+    }
   } catch (error) {
     console.error("Error:", error);
     return NextResponse.json(
-      { message: "Failed to process car", error },
+      { message: "Failed to process car(s)", error },
       { status: 500 }
     );
   }
 }
+
 export async function GET(req: Request) {
   try {
     const url = new URL(req.url);
